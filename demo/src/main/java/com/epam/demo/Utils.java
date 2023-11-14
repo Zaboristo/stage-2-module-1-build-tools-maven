954e579d42b76707d1a0d109d1838265872fd531
boris <baizenbe@gmail.com>
2023-11-14 20:00:44 +0600

    idk?

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -8,6 +8,12 @@ public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         for(String str: args){
             if(str == null) return false;
+            try{
+                Integer.parseInt(str);
+            }
+            catch (Exception e ){
+                return false;
+            }
             if(Integer.parseInt(str) <= 0) return false;
         }
         return true;
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,7 +3,12 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if(str == null) return false;
+        if (str == null) return false;
+        try {
+            Integer.parseInt(str);
+        } catch (Exception e) {
+            return false;
+        }
         return Integer.parseInt(str) > 0;
 
     }
